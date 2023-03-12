79667a5350334f0e764150c5058eb717ea8ae5a4
Mark De Ruyter <mderu001@ucr.edu>
2023-03-11 19:56:11 -0800

    Update subsite.py

--- mkdocs/builder/subsite.py
+++ mkdocs/builder/subsite.py
@@ -5,6 +5,8 @@ from mkdocs.config.defaults import MkDocsConfig
 class SubSite():
     def __init__(self, config: MkDocsConfig):
         self.config = config
+        # TODO: Add a lock around reading/setting built to prevent race conditions
+        # caused by mutliple page loads.
         self.built = False
 
         self._shutdown = False
